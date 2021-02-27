d91b0da0d07984e8406d5de59e76dcaf222ad9e4
Wesley Lima <w.freitas.lima@gmail.com>
2021-02-27 19:10:57 -0300

    Aula 34

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -8,7 +8,7 @@ class Contador extends React.Component {
             numeroInicial: props.numeroInicial || 0,
             numero: props.numeroInicial,
             passo: props.passoInicial || 1
-        }
+        };
     }
 
     add = () => {
@@ -23,20 +23,21 @@ class Contador extends React.Component {
         });
     }
 
-    setPasso = (event) => {
+    setPasso = (evento) => {
         this.setState({
-            passo: +event.target.value
-        })
+            passo: parseInt(evento.target.value)
+        });
     }
 
     render() {
         return (
             <div>
+                <p>Exemplo de componente em classe</p>
                 <h2>Contador</h2>
                 <p>Valor Inicial: {this.state.numeroInicial}</p>
                 <p>Valor Atual: {this.state.numero}</p>
                 <p>
-                    <input type="text" value={this.state.passo} onChange={this.setPasso} />
+                    <input type="number" value={this.state.passo} onChange={this.setPasso} />
                     <button onClick={this.add}>+</button>
                     <button onClick={this.rmv}>-</button>
                 </p>
